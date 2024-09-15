dfacc6ce3b0c74a3f7c524d40ba94edbfafc6692
Tsolangon <160089005+Tsolangon@users.noreply.github.com>
2024-09-15 22:38:24 +0800

    update

--- app/screens/LoginScreen.tsx
+++ app/screens/LoginScreen.tsx
@@ -1,59 +1,45 @@
-import { useState } from 'react';
-import { StyleSheet, TouchableOpacity, View } from 'react-native';
-import { Text } from 'react-native-paper';
+import React, { useState } from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { Text } from "react-native-paper";
 
-import BackButton from '../components/BackButton';
-import Background from '../components/Background';
-import Button from '../components/Button';
-import Header from '../components/Header';
-import Logo from '../components/Logo';
-import TextInput from '../components/TextInput';
-import { theme } from '../core/theme';
-import { emailValidator } from '../helpers/emailValidator';
-import { passwordValidator } from '../helpers/passwordValidator';
+import BackButton from "../components/BackButton";
+import Background from "../components/Background";
+import Button from "../components/Button";
+import Header from "../components/Header";
+import Logo from "../components/Logo";
+import TextInput from "../components/TextInput";
+import { theme } from "../core/theme";
+import { emailValidator } from "../helpers/emailValidator";
+import { passwordValidator } from "../helpers/passwordValidator";
 
-const LoginScreen = ({ navigation }) => {
-  const [email, setEmail] = useState({ value: '', error: '' });
-  const [password, setPassword] = useState({ value: '', error: '' });
+export default function LoginScreen({ navigation }) {
+  const [email, setEmail] = useState({ value: "", error: "" });
+  const [password, setPassword] = useState({ value: "", error: "" });
 
-  const handleLoginPress = () => {
+  const onLoginPressed = () => {
     const emailError = emailValidator(email.value);
     const passwordError = passwordValidator(password.value);
-
     if (emailError || passwordError) {
       setEmail({ ...email, error: emailError });
       setPassword({ ...password, error: passwordError });
       return;
     }
-
     navigation.reset({
       index: 0,
-      routes: [{ name: 'HomeScreen' }],
+      routes: [{ name: "HomeScreen" }],
     });
-  };
-
-  const handleForgotPasswordPress = () => {
-    navigation.navigate('ResetPasswordScreen');
-  };
-
-  const handleCreateAccountPress = () => {
-    navigation.replace('RegisterScreen');
   };
 
   return (
     <Background>
       <BackButton goBack={navigation.goBack} />
       <Logo />
-<<<<<<< HEAD
-      <Header>Sign in</Header>
-=======
-      <Header>Hello, Fellow Traiblazer!</Header>
->>>>>>> f40cd399b3913baf5db5584905ef56f79b02f451
+      <Header>Account Login</Header>
       <TextInput
         label="Email"
         returnKeyType="next"
         value={email.value}
-        onChangeText={(text) => setEmail({ value: text, error: '' })}
+        onChangeText={(text) => setEmail({ value: text, error: "" })}
         error={!!email.error}
         errorText={email.error}
         autoCapitalize="none"
@@ -64,53 +50,40 @@ const LoginScreen = ({ navigation }) => {
         label="Password"
         returnKeyType="done"
         value={password.value}
-        onChangeText={(text) => setPassword({ value: text, error: '' })}
+        onChangeText={(text) => setPassword({ value: text, error: "" })}
         error={!!password.error}
         errorText={password.error}
-<<<<<<< HEAD
         secureTextEntry description={undefined}      />
       <View style={styles.forgotPassword}>
         <TouchableOpacity
           onPress={() => navigation.navigate("ResetPasswordScreen")}
         >
           <Text style={styles.forgot}>Forgot your password ?</Text>
-=======
-        secureTextEntry
-        onSubmitEditing={handleLoginPress} description={undefined}      />
-      <View style={styles.forgotPassword}>
-        <TouchableOpacity onPress={handleForgotPasswordPress}>
-          <Text style={[styles.forgot, { fontWeight: 'bold' }]}>Forgot your password?</Text>
->>>>>>> f40cd399b3913baf5db5584905ef56f79b02f451
         </TouchableOpacity>
       </View>
-      <Button mode="contained" onPress={handleLoginPress} style={undefined}>
+      <Button mode="contained" onPress={onLoginPressed} style={undefined}>
         Log in
       </Button>
       <View style={styles.row}>
         <Text>Don’t Have an Account Yet?</Text>
       </View>
       <View style={styles.row}>
-<<<<<<< HEAD
         <TouchableOpacity onPress={() => navigation.replace("RegisterScreen")}>
           <Text style={styles.link}>Create an Account</Text>
-=======
-        <TouchableOpacity onPress={handleCreateAccountPress}>
-          <Text style={styles.link}>Create</Text>
->>>>>>> f40cd399b3913baf5db5584905ef56f79b02f451
         </TouchableOpacity>
       </View>
     </Background>
   );
-};
+}
 
 const styles = StyleSheet.create({
   forgotPassword: {
-    width: '100%',
-    alignItems: 'flex-end',
+    width: "100%",
+    alignItems: "flex-end",
     marginBottom: 10,
   },
   row: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 4,
   },
   forgot: {
@@ -118,9 +91,7 @@ const styles = StyleSheet.create({
     color: theme.colors.secondary,
   },
   link: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     color: theme.colors.primary,
   },
 });
-
-export default LoginScreen;
