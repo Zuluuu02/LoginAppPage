2ade10ea02fd70231cec0fe2815289ff5ba40dba
Xyrille Uy <uy.xyrille823@gmail.com>
2024-09-16 22:57:31 +0800

    Update StartScreen.tsx

--- app/screens/StartScreen.tsx
+++ app/screens/StartScreen.tsx
@@ -9,25 +9,17 @@ export default function StartScreen({ navigation }) {
   return (
     <Background>
       <Logo />
-
-      {/* Updated header with proper line break */}
       <Header>
         Welcome to USTP{"\n"}   Student Portal
       </Header>
-
-      {/* Updated paragraph content */}
       <Paragraph>
         Home of the Trailblazers
       </Paragraph>
-
-      {/* Button to navigate to the login screen */}
       <Button
         mode="contained"
         onPress={() => navigation.navigate("LoginScreen")} style={undefined}      >
         Log in
       </Button>
-
-      {/* Button to navigate to the register screen */}
       <Button
         mode="outlined"
         onPress={() => navigation.navigate("RegisterScreen")} style={undefined}      >
